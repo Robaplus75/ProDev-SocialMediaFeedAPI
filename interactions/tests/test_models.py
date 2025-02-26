bdd87658a17b6c87de91c04d027b5d732b8bd5ef
robel <robelwon75@gmail.com>
2025-02-26 14:04:13 +0300

    Fix import error in interactions tests

--- interactions/tests/test_models.py
+++ interactions/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from posts.models import Post
-from .models import Interaction
+from ..models import Interaction
 
 
 class InteractionModelTest(TestCase):
