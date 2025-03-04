06b17c0307ea268f8db3bd6848dfdcfdeadc463b
robel <robelwon75@gmail.com>
2025-03-04 21:40:45 +0300

    configured it to use the custom user model instead of the default user model

--- posts/schema/mutations.py
+++ posts/schema/mutations.py
@@ -1,7 +1,9 @@
 import graphene
 from .types import PostType, CommentType
 from ..models import Post, Comment
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class CreatePost(graphene.Mutation):
