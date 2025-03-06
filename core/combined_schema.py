2aa389d3ad11a472e817ac5882b913c1b2397171
robel <robelwon75@gmail.com>
2025-03-06 14:23:35 +0300

    added the user's Query and Mutation to the combined schema

--- core/combined_schema.py
+++ core/combined_schema.py
@@ -1,18 +1,25 @@
 import graphene
 
+from users.schema.queries import Query as UsersQuery
 from posts.schema.queries import Query as PostsQuery
 from interactions.schema.queries import Query as InteractionsQuery
 
+from users.schema.mutations import Mutation as UsersMutation
 from posts.schema.mutations import Mutation as PostsMutation
 from interactions.schema.mutations import Mutation as InteractionsMutation
 
 
-class Query(PostsQuery, InteractionsQuery, graphene.ObjectType):
+class Query(UsersQuery, PostsQuery, InteractionsQuery, graphene.ObjectType):
     """Combined query class for posts and interactions."""
     pass
 
 
-class Mutation(PostsMutation, InteractionsMutation, graphene.ObjectType):
+class Mutation(
+        UsersMutation,
+        PostsMutation,
+        InteractionsMutation,
+        graphene.ObjectType
+):
     """Combined mutation class for posts and interactions."""
     pass
 
