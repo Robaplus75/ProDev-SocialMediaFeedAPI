0ec16cff5ca2da81b4af4aefb0c7aef77e32d1ea
robel <robelwon75@gmail.com>
2025-03-06 17:06:58 +0300

    updated the tests to use the custom user model

--- posts/tests/test_models.py
+++ posts/tests/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from ..models import Post, Comment
+
+User = get_user_model()
 
 
 class PostModelTest(TestCase):
