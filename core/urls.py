ad956fdfb2e74c042406bf92ada70ed2ff15b9e6
robel <robelwon75@gmail.com>
2025-03-07 14:51:32 +0300

    replaced the GraphQLView with FileUploadGraphqlview to support file uploads

--- core/urls.py
+++ core/urls.py
@@ -17,11 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
+from graphene_file_upload.django import FileUploadGraphQLView
 from graphql_playground.views import GraphQLPlaygroundView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql/",
+         csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
     path('playground/', GraphQLPlaygroundView.as_view(endpoint="/graphql/")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+            settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT
+    )
